674d3a37198b7a27e8586db5872dbacf2bfc30ee
erikaOG <eortizg@institutobenitojuarez.edu.ec>
2017-12-18 22:39:33 -0500

    Erika Ortiz
    Ejemplo de Open Close

---
+++ OCGeometricas/src/OCGeometricas/Circulo.java
@@ -0,0 +1,40 @@
+/*
+ * Circulo
+ */
+package OCGeometricas;
+
+/**
+ * @author www.ajpdsoft.com
+ */
+public class Circulo implements FiguraGeometrica
+{
+    private double radio;
+    private double areaRadio;
+    static final double pi = 3.1415;
+
+    public Circulo(double radio) 
+    {
+        this.radio = radio;
+    }
+
+    public double getRadio() 
+    {
+        return radio;
+    }
+
+    public void setRadio(double radio) 
+    {
+        this.radio = radio;
+    }
+
+    public double getAreaRadio() 
+    {
+        return areaRadio;
+    }
+
+    @Override
+    public void area() 
+    {
+        areaRadio = pi * radio * radio;
+    }    
+}
---
+++ OCGeometricas/src/OCGeometricas/Cuadrado.java
@@ -0,0 +1,39 @@
+/*
+ * Cuadrado
+ */
+package OCGeometricas;
+
+/**
+ * @author www.ajpdsoft.com
+ */
+public class Cuadrado implements FiguraGeometrica
+{
+    private double lado;
+    private double areaCuadrado;
+
+    public Cuadrado(double lado) 
+    {
+        this.lado = lado;
+    }
+    
+    public double getLado() 
+    {
+        return lado;
+    }
+
+    public void setLado(double lado) 
+    {
+        this.lado = lado;
+    }
+
+    public double getareaCuadrado() 
+    {
+        return areaCuadrado;
+    }    
+    
+    @Override
+    public void area() 
+    {
+        areaCuadrado = lado * lado;
+    }    
+}
